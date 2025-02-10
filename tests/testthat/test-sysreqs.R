a6b35750a2678ed3a3854927d8cd51b79758b6cb
Robert Davey <robertdavey@carpentries.org>
2025-02-10 14:58:52 +0000

    Pin 22.04, skip test-sysreqs on windows and mac

--- tests/testthat/test-sysreqs.R
+++ tests/testthat/test-sysreqs.R
@@ -14,6 +14,8 @@ test_that("ci_sysreqs will detect the requirements for a knitr lockfile", {
 
 test_that("ci_sysreqs will skip reqs if requested", {
   skip_if_offline()
+  skip_on_os("windows")
+  skip_on_os("mac")
   on_ubuntu <- grepl("ubuntu", system("lsb_release -irs", intern = TRUE)[[1]], ignore.case = TRUE)
   skip_if_not(on_ubuntu)
   lock <- system.file("renv.lock", package = "vise")
